58aa78cb7432d89b379e68001d4c8d843a78cafb
Chris Evans <chrisevans9629@gmail.com>
2019-11-26 17:21:29 -0600

    Add project files.

---
+++ DbFinalExam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbFinalExam
+{
+    public static class Connection
+    {
+        public static string Database { get; set; }
+        public static string Server { get; set; }
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        public static string ConnectionStr => $@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+
+    static class Program
+    {
+        
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
